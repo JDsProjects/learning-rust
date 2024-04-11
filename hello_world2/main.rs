a51e92bfb94dc40da13ef6eda427fac4c98d27a8
JDJG <ghostzap123@gmail.com>
2024-04-10 20:13:19 -0400

    Create main.rs

---
+++ hello_world2/main.rs
@@ -0,0 +1,15 @@
+fn main(){
+    println!("Hello World");
+    println!("How are you doing today?");
+    
+    // Very Much like Java Comments
+    
+    /*
+    Hey this is a multilined comment
+    */
+    
+    println!("Comments work YEAHHHHHHHHHHHHHHHHHHHHHHHHH");
+    
+    // rustfmt (no use of the formatter yet.)
+    // is main function always used or is it the name of the file?
+}
