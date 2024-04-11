a71f77262ea68c47eace877117354570297e5b38
JDJG <ghostzap123@gmail.com>
2024-04-10 20:21:10 -0400

    Create hello_world3.rs

---
+++ hello_world3.rs
@@ -0,0 +1,19 @@
+fn main(){
+
+    println!("Hello World");
+    // fn is to define a function
+    // requires main()
+    // main is the function name
+    /* 
+    Define multi block
+    */
+    
+    // variable time?
+    
+    // doc comments? I am going to ignore that for now.
+    // https://learning-rust.github.io/docs/comments-and-documenting-the-code/
+    // (going to ignore going to the next for now.)
+    
+    
+    
+}
